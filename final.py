fda6a85490e36a1da114c0a40e6cf315b50a3381
Spencer Kam <sbkam98@gmail.com>
2019-06-05 15:40:46 -0700

    Added some real time functionality.  ET calc is also no longer cummulative unless there is a delay

--- final.py
+++ final.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 import time
 
-currentSize = 0
+currentSize = 0 #of CIMIS data
 maxLines = 100
 data = []
 avgET = 0.0
+realTime = datetime.now()
+hours = 0
 
 class dataNode:
 	def __init__(self, time, ETo, temp, humidity):
@@ -18,54 +21,77 @@ class dataNode:
 
 def loop():
 	global currentSize
+	global realTime
+	global hours
+	localTemp = 0.0
+	localHumidity = 0
+	minutes = 0
+
 	while(currentSize < 10):
 		#First Get the local humidity and temp. Store into data structure
 		#newNode = dataNode(l_time, l_ETo, l_temp, l_humidity)
-		newNode = dataNode(100, 0.01, 70.0, 70)
-		data.append(newNode)
-	
-		#Next get data from CIMIS and read the resulting file
-		count = 0
-		sum   = 0.0
-		with open("hourly075.csv", "rt") as file:
-			for line in file:
-				info = line.split(",")
-				if count >= len(data):
-					break
-			
-				if info[4] == "--":
-					break
-				
-				currentTime     = info[2]
-				ETo      = float(info[4])
-				temp     = float(info[12])
-				humidity = info[14]
+		now = realTime.now()
+		currentMinutes = int(now.strftime("%S"))
+		if currentMinutes != minutes:
+			#print "Adding a minute"
+			minutes += 1
+			localTemp += 70.0
+			localHumidity += 70
 
-				print "-----Index: %d" %(count)
-				print "ETo: " + str(ETo)
-				print "temp: " + str(temp)
-				print "humidity: " + str(humidity)
+		if minutes == 60:
+			hours += 1
+			minutes = 0
+			averageTemp = localTemp/60.0
+			localTemp = 0.0
+			averageHumidity = localHumidity/60
+			localHumidity = 0
+			newNode = dataNode(100, 0.01, averageTemp, averageHumidity)
+			data.append(newNode)
+	
+			#Next get data from CIMIS and read the resulting file
+			count = 0
+			sum   = 0.0
+			with open("hourly075.csv", "rt") as file:
+				for line in file:
+					info = line.split(",")
+					#stop reading the file either when we have caught up to the specified hours or when we have reaced end of CIMIS file
+					if count >= hours or info[4] == "--":
+						break
 			
-				#If a new entry has info, we can use that to calculate the ET
-				ET = data[count].ETo * float(data[count].temp/temp) * float(humidity)/float(data[count].humidity)
-				data[count].setET(ET)
+					CimisTime     = int(info[2])
+					ETo      = float(info[4])
+					temp     = float(info[12])
+					humidity = int(info[14])
+					count += 1 #increment count everytime there is an entry that isnt empty
+
+					print "-----Index: %d" %(count)
+					print "ETo: " + str(ETo)
+					print "temp: " + str(temp)
+					print "humidity: " + str(humidity)
 			
-				sum += ET
-				count+=1  #increment count everytime there is a new entry that isnt empty
+					#If a new entry has info, we can use that to calculate the ET
+					if count > currentSize:
+						index = count - currentSize - 1
+						ET = data[index].ETo * float(data[index].temp/temp) * float(humidity)/float(data[index].humidity)
+						#data[index].setET(ET)
+						data.pop(index)
+						sum += ET
 			
-		
-		#Calculate average ET.
-		avgET = float(sum/count)
-		print "========= Average: %f" %(avgET)
-			
-		#update currentSize to reflect how many CIMIS entries we have now completed.
-		if(currentSize < count):
-			currentSize = count
+			#CurrentSize holds the length of what the CIMIS data is/was supposed to be, so if count > currentSize, this means new data has been added to CIMIS file.
+			if(currentSize < count):
+				#Calculate average ET.
+				avgET = float(sum/(count-currentSize))
+				print "========= Average: %f" %(avgET)
+				print "========= Hours calc'ed for: %d" %(count-currentSize)
+				print "========= Size of local: %d" %(len(data))
+				currentSize = count
+			#minutes is rest always	
+			minutes = 0
 		
 			
 		#wait another hour to check
-		time.sleep(2)
-		print "Waited"
+		#time.sleep(2)
+		#print "Waited"
 			
 
 if __name__=='__main__':
