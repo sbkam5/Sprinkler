19cfb0889fd831bc119fdf452f0747d3241dc2dd
Manuel <mmacedon@uci.edu>
2019-06-06 01:21:01 -0700

    Added MainController as primary control flow Broke final.py into smaller functions No Clue what final.pyc is, but its there

---
+++ MainController.py
@@ -0,0 +1,102 @@
+#!python2
+#############################################################################
+# Creator: Manuel Macedonio Alvarez
+# Date Created: 06/05/2019
+# Last Modified: 06/05/2019
+# Description: Primary Control Center. Mainloop will track time, interrupts,
+#              Input and Output
+# Revisions:
+# Additional Comments:
+#############################################################################
+
+from datetime import datetime
+import time as clock
+import final as Final
+import threading as t
+
+#functionality for the timer.
+timer_finished = True #Determines whether timer is running
+seconds = 0.5 #Predetermined wait time for the timer
+motion_sensor_detect = False #Bool to determine if someone is present
+current_time = datetime.now()
+stall = True
+
+#Threading Function for the Timer
+def Timer ():
+    global timer_finished
+    global seconds
+    global stall
+    global count
+
+    while ( True ):
+        stall = True
+        print("Timer Start")
+        clock.sleep(seconds) #Timer is Running
+        timer_finished = True
+        while ( stall ): #Wait for Signal from Main Controller to reset timer
+            continue
+
+def Minute_Reset():
+    time = current_time.now() #Get the Current Time
+    current_minutes = int (time.strftime("%S"))
+    minute_counter = 60 - current_minutes #Reset the Minute Counter for the next hour
+    return minute_counter
+
+#Main Controller. Loop and Data Signals
+def Controller():
+    global timer_finished
+    global motion_sensor_detect
+    global current_time
+    global stall
+    global count
+    minute_counter = Minute_Reset() #Keeps track of how many minutes left until an hour
+    hours = 0
+
+    Timer_Thread = t.Thread(target=Timer)
+    Timer_Thread.daemon = True
+    Timer_Thread.start()
+
+    #Main Loop
+    while ( hours < 24 ): #Run for twenty four hours
+
+        timer_finished = False #Global Variable to indicate that the timer is currently running
+        clock.sleep(0.25)
+
+        while ( not timer_finished and not motion_sensor_detect ): #Wait for Timer or Motion Sensor Interrupt
+            #Have LCD do something, so we're not busy waiting
+            continue
+
+        if ( timer_finished ): #Timer Finished
+            print("Timer Finished\n")
+            minute_counter = minute_counter - 1 #Decrement Current Minutes
+            print("Minutes Remaining: " + str(minute_counter))
+            #Read Temperature and Sensor Data
+
+            if ( minute_counter == 0 ): #If time reaches an hour, update values
+                print("Hour Complete: Updating and Reseting")
+                Final.Update(10, 15) #Call the Update Function
+                minute_counter = Minute_Reset() #Reset the amount of minutes left for an hour
+                hours = hours + 1
+
+            stall = False #Signal the Timer thread to restart
+
+        elif ( motion_sensor_detect ): #Motion Sensor detected movement
+            print("Some dumbass walked into the path. Probably Spencer")
+            print("Turning off Relay")
+            motion_sensor_detect = False
+
+    print("24 Hours have passed. Shutting down system\n")
+    exit()
+
+#Main Function Used to Initalize the Program
+def main():
+    print("Starting Sprinkler Control System")
+    try:
+        print("Entering Timer Loop")
+        Controller()
+    except KeyboardInterrupt:
+        print("Received KeyboardInterrupt Shutdown Signal")
+        print("Exiting Program"
+        )
+if __name__ == '__main__':
+    main()
--- final.py
+++ final.py
@@ -1,3 +1,15 @@
+#!python2
+#############################################################################
+# Creator: Spencer Brendan Kam
+# Date Created: idk, go ask him
+# Last Modified: 06/5/2019
+# Description: Module responsible for updating the CIMIS and local data
+# Revisions: Added timing functionality and comment for easier readability
+#  			 Also removed dependencies on previous inputs
+#			 Separated and modularized
+# Additional Comments:
+#############################################################################
+
 from datetime import datetime
 import time
 
@@ -8,16 +20,65 @@ avgET = 0.0
 realTime = datetime.now()
 hours = 0
 
+"""
+	Data structure responsible for keeping track of the ET, local temp and
+	humidity for time t
+"""
 class dataNode:
 	def __init__(self, time, ETo, temp, humidity):
 		self.time     = time
 		self.ETo      = ETo
 		self.temp     = temp
 		self.humidity = humidity
-		
+
 	def setET(self, ET):
 		self.ET       = ET
-	
+
+def Update( localTemp, localHumidity ):
+	global currentSize
+	averageTemp = localTemp / 60.0
+	averageHumidity = localHumidity / 60.0
+	newNode = dataNode( 100, 0.01, averageTemp, averageHumidity )
+	data.append(newNode)
+
+	print("Currently Updating\n")
+	count = 0
+	sum = 0.0
+
+	with open("hourly075.csv", "rt") as file:
+		for line in file:
+			info = line.split(",")
+			#stop reading the file either when we have caught up to the specified hours or when we have reaced end of CIMIS file
+			if count >= hours or info[4] == "--":
+				break
+
+			CimisTime     = int(info[2])
+			ETo      = float(info[4])
+			temp     = float(info[12])
+			humidity = int(info[14])
+			count += 1 #increment count everytime there is an entry that isnt empty
+
+			print "-----Index: %d" %(count)
+			print "ETo: " + str(ETo)
+			print "temp: " + str(temp)
+			print "humidity: " + str(humidity)
+
+			#If a new entry has info, we can use that to calculate the ET
+			if count > currentSize:
+				index = count - currentSize - 1
+				ET = data[index].ETo * float(data[index].temp/temp) * float(humidity)/float(data[index].humidity)
+				data.pop(index)
+				sum += ET
+
+			#CurrentSize holds the length of what the CIMIS data is/was supposed to be,
+			#so if count > currentSize, this means new data has been added to CIMIS file.
+			if(currentSize < count):
+				#Calculate average ET.
+				avgET = float(sum/(count-currentSize))
+				print "========= Average: %f" %(avgET)
+				print "========= Hours calc'ed for: %d" %(count-currentSize)
+				print "========= Size of local: %d" %(len(data))
+				currentSize = count
 
 def loop():
 	global currentSize
@@ -28,76 +89,25 @@ def loop():
 	minutes = 0
 
 	while(currentSize < 10):
-		#First Get the local humidity and temp. Store into data structure
-		#newNode = dataNode(l_time, l_ETo, l_temp, l_humidity)
-		now = realTime.now()
+		now = realTime.now() #Get the Current real time and convert to minutes
 		currentMinutes = int(now.strftime("%S"))
-		if currentMinutes != minutes:
-			#print "Adding a minute"
+		if currentMinutes != minutes: #Check if the minute changed and update the counter if necessary
 			minutes += 1
-			localTemp += 70.0
-			localHumidity += 70
+			localTemp += 70.0 #Static Testing Values
+			localHumidity += 70 #Static Testing Values
 
-		if minutes == 60:
+		if minutes == 60: #If the minute tracker has reached 60 (an hour) -> update
 			hours += 1
 			minutes = 0
-			averageTemp = localTemp/60.0
-			localTemp = 0.0
-			averageHumidity = localHumidity/60
-			localHumidity = 0
-			newNode = dataNode(100, 0.01, averageTemp, averageHumidity)
-			data.append(newNode)
-	
-			#Next get data from CIMIS and read the resulting file
-			count = 0
-			sum   = 0.0
-			with open("hourly075.csv", "rt") as file:
-				for line in file:
-					info = line.split(",")
-					#stop reading the file either when we have caught up to the specified hours or when we have reaced end of CIMIS file
-					if count >= hours or info[4] == "--":
-						break
-			
-					CimisTime     = int(info[2])
-					ETo      = float(info[4])
-					temp     = float(info[12])
-					humidity = int(info[14])
-					count += 1 #increment count everytime there is an entry that isnt empty
+			Update( localTemp, localHumidity ) #Run calculations and Update CIMIS
 
-					print "-----Index: %d" %(count)
-					print "ETo: " + str(ETo)
-					print "temp: " + str(temp)
-					print "humidity: " + str(humidity)
-			
-					#If a new entry has info, we can use that to calculate the ET
-					if count > currentSize:
-						index = count - currentSize - 1
-						ET = data[index].ETo * float(data[index].temp/temp) * float(humidity)/float(data[index].humidity)
-						#data[index].setET(ET)
-						data.pop(index)
-						sum += ET
-			
-			#CurrentSize holds the length of what the CIMIS data is/was supposed to be, so if count > currentSize, this means new data has been added to CIMIS file.
-			if(currentSize < count):
-				#Calculate average ET.
-				avgET = float(sum/(count-currentSize))
-				print "========= Average: %f" %(avgET)
-				print "========= Hours calc'ed for: %d" %(count-currentSize)
-				print "========= Size of local: %d" %(len(data))
-				currentSize = count
-			#minutes is rest always	
-			minutes = 0
-		
-			
 		#wait another hour to check
-		#time.sleep(2)
-		#print "Waited"
-			
+
 
 if __name__=='__main__':
 	print('Program is starting')
-	
-	
+
+
 	try:
 		loop()
 	except KeyboardInterrupt:
