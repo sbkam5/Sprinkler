57360be391702002d192e202deed50fe08a73b48
Entropy Xu <entropy.xcy@protonmail.com>
2019-06-11 15:09:01 -0700

    Add Irvine to be a station

--- cimis/cimis_test.py
+++ cimis/cimis_test.py
@@ -3,7 +3,7 @@ from cimis import *
 
 def main():
     appKey = 'acac78e2-860f-4194-b27c-ebc296745833'  # cimis unique appKey
-    sites = [80]  #query single site or multiple
+    sites = [75]  #query single site or multiple
     xls_path = 'CIMIS_query.xlsx' # TODO: make this dep on stations/query date
 
     interval ='hourly' #options are:    default, daily, hourly
@@ -15,7 +15,7 @@ def main():
 
     write_output_file(xls_path, cimis_data, site_names)
 
-    # print(site_names, cimis_data)
+    print(site_names, cimis_data)
 
 
 if __name__ == "__main__":
