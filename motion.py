4f47c7b03b63ae1c5965985e6da88887ecb7feb2
Tom Wang <42152021+YutongWang-Tom@users.noreply.github.com>
2019-06-11 17:21:28 -0700

    motion sensor

---
+++ motion.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIOs
+
+ledPin = 12
+sensorPin =11
+
+ispassing = False
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin,GPIO.OUT)
+    GPIO.setup(sensorPin, GPIO.IN)
+
+def loop(){
+    while True:
+        if GPIO.input(sensorPin)==GPIO.HIGH:
+            GPIO.output(ledPin,GPIO.HIGH)
+            ispassing = True
+        else:
+            GPIO.output(ledPin,GPIO.LOW)
+            ispassing = False
+}
+
+def destroy():
+    GPIO.cleanup()
+
+
+if __name__== '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
