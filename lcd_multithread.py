da56a4dbc26486e031e6dfa1f277a5390799cb11
Tom Wang <42152021+YutongWang-Tom@users.noreply.github.com>
2019-06-11 17:07:40 -0700

    lcd works, check lcd_multithread

--- lcd_multithread.py
+++ lcd_multithread.py
@@ -4,7 +4,7 @@
 from PCF8574 import PCF8574_GPIO
 from Adafruit_LCD1602 import Adafruit_CharLCD
 
-import multiprocessing
+import threading
 import time
 
 title = "BRUH:"
@@ -19,14 +19,19 @@ def updateString(s):
     lcd.clear()
     time.sleep(1)
 
-def str_loop(length):
+def str_loop(l):
     global update
+    global buf
+    global length
+    global counter
+    length = l
     counter = 15
     if length>16:
         buf = string[0:15]
         while True:
             #lcd.delayMicroseconds(200)
             print ("loop:"+string)
+            print("counter",counter)
             lcd.clear()
             lcd.message(title+'\n')
             lcd.message(buf)
@@ -36,13 +41,14 @@ def str_loop(length):
             if counter>=length:
                 counter=0
             if update:
+                counter=15
+                buf = string[0:15]
+                length = len(string)
                 update = False
-                break
     else:
         buf = string
         lcd.message(title+'\n')
         lcd.message(string)
-
 
 
 def LCDprint(s):
@@ -73,3 +79,13 @@ except:
         exit(1)
 #create LCD, passing in MCP GPIO adapter
 lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
+x = threading.Thread(target=LCDprint,args=(string,))
+x.start()
+time.sleep(3)
+print("wake up")
+updateString("bufer is working yay buffer is working !!")
+time.sleep(5)
+print("wake up")
+updateString("MERP MERP MERP MERP MERP MERP")
+x.join()
